7a284a8a1b5d68a7db14a5f223e4701aa7979ef0
GuiCandini <guilhermecandini@hotmail.com>
2024-08-06 17:07:22 -0300

    Exercício 05/08

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { config } from "dotenv";
+import mongoose from "mongoose";
+import { contatosRouter } from "./routes/contatos.js";
+import { usuariosRouter } from "./routes/usuarios.js";
+
+config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongo DB conectado!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(express.json()); //configuração para fazer a requisição do body usando o json
+
+app.use(contatosRouter);
+app.use(usuariosRouter);
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
---
+++ models/contato.js
@@ -0,0 +1,26 @@
+import {model, Schema} from "mongoose";
+
+export const Contato = model("contato", new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    sobrenome: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    telefone: {
+        type: String,
+        unique: true
+    },
+    observacoes: {
+        type: String
+    },
+    favorito: {
+        type: Boolean,
+        default: false
+    }
+}));
